4855024ca83ef0022130b53d44d812736ba9e7a8
Gabriel Cordova Medeiros <gabiceemi@gmail.com>
2023-04-22 17:56:11 -0300

    all changes

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Gabriel Medeiros',
+      email: 'gabriel.medeiros@gmail.com',
+      avatarUrl: 'https://github.com/gabiceemi.png',
+    }
+  })
+
+  const poll = await prisma.poll.create({
+    data: {
+      title: 'Example Poll 1',
+      code: 'BOL123',
+      ownerId: user.id,
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-08T16:00:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'DE',
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-10T14:30:00.201Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 3,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_pollId: {
+                userId: user.id,
+                pollId: poll.id,
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+
+}
+
+main()
---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+
+import { pollRoutes } from "./routes/poll";
+import { userRoutes } from "./routes/user";
+import { guessRoutes } from "./routes/guess";
+import { gameRoutes } from "./routes/game";
+import { authRoutes } from "./routes/auth";
+
+// singleton -> reaproveita varios arquivos ja executados
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    });
+
+    await fastify.register(cors, {
+        origin: true    // para ambientes em produção geralmente informamos o host que vai acessar o backend
+    });
+
+    // MOVER SECRETKEY PARA VARIAVEIS DE AMBIENTE
+    await fastify.register(jwt, {
+        secret: 'nlwcopa2023secrettoken',
+    });
+
+    await fastify.register(authRoutes);
+    await fastify.register(pollRoutes);
+    await fastify.register(userRoutes);
+    await fastify.register(guessRoutes);
+    await fastify.register(gameRoutes);
+   
+    await fastify.listen({ port: 3333, host: '0.0.0.0' });
+}
+
+bootstrap()
